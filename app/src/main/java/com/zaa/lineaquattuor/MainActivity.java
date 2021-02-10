a9ce7c094790574173160eaf49a804adff858248
Zaa <robni7@me.com>
2021-02-10 16:01:01 +0100

    Assignment 3.0: stukje code overtypen.

--- app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
+++ app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
@@ -3,12 +3,22 @@ package com.zaa.lineaquattuor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView infoTextView;
+
+    private void info(String info) {
+        infoTextView.setText(info);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        infoTextView = findViewById(R.id.info_textview); // Haalt de XML-textview op
+        info(" Player one is up."); // Betekent zoveel als infoTextView.setText(" Player etc."); Quick hack: spatie voor het bericht tbv gebrek aan padding.
     }
+
 }
