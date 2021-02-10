d237deb5a7774c317daa2c2c13225a9d1a24b4da
Zaa <robni7@me.com>
2021-02-10 18:07:04 +0100

    Extra lay-out

--- app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
+++ app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
@@ -2,7 +2,10 @@ package com.zaa.lineaquattuor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -12,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView infoTextView;
     private TableLayout boardTableLayout;
+    private Button columnButton;
+    private int column;
+    private int mycolor = Color.RED;
+
 
     private void info(String theInfo) {
         infoTextView.setText(theInfo);
@@ -26,12 +33,27 @@ public class MainActivity extends AppCompatActivity {
         getBoardImageView(theRow - 1, theColumn - 1).setBackgroundColor(theColor);
     }
 
+    public void columnButtonClicked(View theView) {
+        columnButton = (Button) theView;
+        column = Integer.parseInt(columnButton.getText().toString());
+        // further code to make the move and process the row returned
+        if (mycolor == Color.RED) {
+            showDisc(6, column, mycolor);
+            mycolor = Color.BLUE;
+            info("All eyes on you, player two! ");
+        } else {
+            showDisc(6, column, mycolor);
+            mycolor = Color.RED;
+            info("Show ’em how it’s done, player one!");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         infoTextView = findViewById(R.id.info_textview); // Haalt de XML-textview op
-        info("Player one is up."); // Betekent zoveel als infoTextView.setText(" Player etc.");
+        info("Player one, you’re up!"); // Betekent zoveel als infoTextView.setText(" Player etc.");
         boardTableLayout = findViewById(R.id.board_tablelayout); // Het speelbord, dus de tabel uit de XML
 
     }
