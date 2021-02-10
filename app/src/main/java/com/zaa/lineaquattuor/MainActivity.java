bdb2d752d8e9c6524d95128c0f1f8d2cb7888dcd
Zaa <robni7@me.com>
2021-02-10 17:22:20 +0100

    Added images and configured src in table

--- app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
+++ app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
@@ -3,14 +3,27 @@ package com.zaa.lineaquattuor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView infoTextView;
+    private TableLayout boardTableLayout;
 
-    private void info(String info) {
-        infoTextView.setText(info);
+    private void info(String theInfo) {
+        infoTextView.setText(theInfo);
+    }
+
+    private ImageView getBoardImageView(int theRowIndex, int theColumnIndex) {
+        TableRow tableRow = (TableRow) boardTableLayout.getChildAt(theRowIndex);
+        return (ImageView) tableRow.getChildAt(theColumnIndex);
+    }
+
+    private void showDisc(int theRow, int theColumn, int theColor) { // set the background color of specified cell
+        getBoardImageView(theRow - 1, theColumn - 1).setBackgroundColor(theColor);
     }
 
     @Override
@@ -19,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         infoTextView = findViewById(R.id.info_textview); // Haalt de XML-textview op
         info(" Player one is up."); // Betekent zoveel als infoTextView.setText(" Player etc."); Quick hack: spatie voor het bericht tbv gebrek aan padding.
+        boardTableLayout = findViewById(R.id.board_tablelayout); // Het speelbord, dus de tabel uit de XML
+
     }
 
 }
