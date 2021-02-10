d3d0695c69891a7ce8d14830f5b3737f1ff9d55f
Zaa <robni7@me.com>
2021-02-10 19:28:20 +0100

    Experimenteren met kleuren

--- app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
+++ app/src/main/java/com/zaa/lineaquattuor/MainActivity.java
@@ -1,8 +1,10 @@
 package com.zaa.lineaquattuor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +19,15 @@ public class MainActivity extends AppCompatActivity {
     private TableLayout boardTableLayout;
     private Button columnButton;
     private int column;
-    private int mycolor = Color.RED;
+    private int mycolor;
+
+if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        mycolor = (getColor(R.color.teal_200));
+    } else {
+        mycolor = (getResources().getColor(R.color.teal_200));
+    }
+
+    private String columnLetter;
 
 
     private void info(String theInfo) {
@@ -35,15 +45,41 @@ public class MainActivity extends AppCompatActivity {
 
     public void columnButtonClicked(View theView) {
         columnButton = (Button) theView;
-        column = Integer.parseInt(columnButton.getText().toString());
+
+        // Perhaps a clunky way of sorting this through
+        columnLetter = columnButton.getText().toString();
+        switch (columnLetter) {
+            case "A":
+                column = 1;
+                break;
+            case "B":
+                column = 2;
+                break;
+            case "C":
+                column = 3;
+                break;
+            case "D":
+                column = 4;
+                break;
+            case "E":
+                column = 5;
+                break;
+            case "F":
+                column = 6;
+                break;
+            case "G":
+                column = 7;
+                break;
+        }
+
         // further code to make the move and process the row returned
-        if (mycolor == Color.RED) {
+        if (mycolor == ContextCompat.getColor(getResources(), R.color.teal_200)) {
             showDisc(6, column, mycolor);
-            mycolor = Color.BLUE;
+            mycolor = ContextCompat.getColor(getResources(), R.color.aubergine_200);
             info("All eyes on you, player two! ");
         } else {
             showDisc(6, column, mycolor);
-            mycolor = Color.RED;
+            mycolor = ContextCompat.getColor(getResources(), R.color.teal_200);
             info("Show ’em how it’s done, player one!");
         }
     }
